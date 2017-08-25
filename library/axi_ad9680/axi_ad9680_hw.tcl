holodnak/hdl

axi_ad9680: Use the generic JESD204 ADC receiver core
Replace the axi_ad9680 implementation with the new generic JESD204
interface ADC core. The replacement is functionally equivalent.

Signed-off-by: Lars-Peter Clausen <lars@metafoo.de>

library/axi_ad9680/axi_ad9680_hw.tcl
@@ -25,9 +25,13 @@ add_fileset_file up_delay_cntrl.v     VERILOG PATH $ad_hdl_dir/library/common/up
 add_fileset_file up_adc_common.v      VERILOG PATH $ad_hdl_dir/library/common/up_adc_common.v
 add_fileset_file up_adc_channel.v     VERILOG PATH $ad_hdl_dir/library/common/up_adc_channel.v
 add_fileset_file ad_xcvr_rx_if.v      VERILOG PATH $ad_hdl_dir/library/common/ad_xcvr_rx_if.v
-add_fileset_file axi_ad9680_pnmon.v   VERILOG PATH axi_ad9680_pnmon.v
-add_fileset_file axi_ad9680_channel.v VERILOG PATH axi_ad9680_channel.v
-add_fileset_file axi_ad9680_if.v      VERILOG PATH axi_ad9680_if.v
+
+add_fileset_file axi_adc_jesd204_pnmon.v   VERILOG PATH $ad_hdl_dir/library/axi_dac_jesd204/axi_adc_jesd204_pnmon.v
+add_fileset_file axi_adc_jesd204_channel.v VERILOG PATH $ad_hdl_dir/library/axi_dac_jesd204/axi_dac_jesd204_channel.v
+add_fileset_file axi_adc_jesd204_core.v    VERILOG PATH $ad_hdl_dir/library/axi_dac_jesd204/axi_dac_jesd204_core.v
+add_fileset_file axi_adc_jesd204_if.v      VERILOG PATH $ad_hdl_dir/library/axi_dac_jesd204/axi_dac_jesd204_if.v
+add_fileset_file axi_adc_jesd204.v         VERILOG PATH $ad_hdl_dir/library/axi_dac_jesd204/axi_dac_jesd204.v
+
 add_fileset_file axi_ad9680.v         VERILOG PATH axi_ad9680.v TOP_LEVEL_FILE
 add_fileset_file ad_axi_ip_constr.sdc SDC     PATH $ad_hdl_dir/library/common/ad_axi_ip_constr.sdc
 

topic-embedded-products/adi_hdl

library/axi_ad9680/axi_ad9680_hw.tcl
@@ -25,9 +25,13 @@ add_fileset_file up_delay_cntrl.v     VERILOG PATH $ad_hdl_dir/library/common/up
 add_fileset_file up_adc_common.v      VERILOG PATH $ad_hdl_dir/library/common/up_adc_common.v
 add_fileset_file up_adc_channel.v     VERILOG PATH $ad_hdl_dir/library/common/up_adc_channel.v
 add_fileset_file ad_xcvr_rx_if.v      VERILOG PATH $ad_hdl_dir/library/common/ad_xcvr_rx_if.v
-add_fileset_file axi_ad9680_pnmon.v   VERILOG PATH axi_ad9680_pnmon.v
-add_fileset_file axi_ad9680_channel.v VERILOG PATH axi_ad9680_channel.v
-add_fileset_file axi_ad9680_if.v      VERILOG PATH axi_ad9680_if.v
+
+add_fileset_file axi_adc_jesd204_pnmon.v   VERILOG PATH $ad_hdl_dir/library/axi_adc_jesd204/axi_adc_jesd204_pnmon.v
+add_fileset_file axi_adc_jesd204_channel.v VERILOG PATH $ad_hdl_dir/library/axi_adc_jesd204/axi_adc_jesd204_channel.v
+add_fileset_file axi_adc_jesd204_core.v    VERILOG PATH $ad_hdl_dir/library/axi_adc_jesd204/axi_adc_jesd204_core.v
+add_fileset_file axi_adc_jesd204_if.v      VERILOG PATH $ad_hdl_dir/library/axi_adc_jesd204/axi_adc_jesd204_if.v
+add_fileset_file axi_adc_jesd204.v         VERILOG PATH $ad_hdl_dir/library/axi_adc_jesd204/axi_adc_jesd204.v
+
 add_fileset_file axi_ad9680.v         VERILOG PATH axi_ad9680.v TOP_LEVEL_FILE
 add_fileset_file up_xfer_cntrl_constr.sdc   SDC PATH  $ad_hdl_dir/library/altera/common/up_xfer_cntrl_constr.sdc
 add_fileset_file up_xfer_status_constr.sdc  SDC PATH  $ad_hdl_dir/library/altera/common/up_xfer_status_constr.sdc
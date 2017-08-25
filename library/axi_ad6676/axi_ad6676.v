holodnak/hdl

Remove unused IO_DELAY_GROUP parameter from JESD204 ADC cores
The cores that handle the JESD204 ADC converters do not feature any direct
IO and subsequently no IO-delay blocks either. Remove the unused
IO_DELAY_GROUP parameter.

Signed-off-by: Lars-Peter Clausen <lars@metafoo.de>

library/axi_ad6676/axi_ad6676.v
@@ -42,8 +42,7 @@
 module axi_ad6676 #(
 
   parameter ID = 0,
-  parameter DEVICE_TYPE = 0,
-  parameter IO_DELAY_GROUP = "adc_if_delay_group") (
+  parameter DEVICE_TYPE = 0) (
 
   // jesd interface
   // rx_clk is (line-rate/40)

library/axi_ad9625/axi_ad9625.v
@@ -40,8 +40,7 @@
 module axi_ad9625 #(
 
   parameter ID = 0,
-  parameter DEVICE_TYPE = 0,
-  parameter IO_DELAY_GROUP = "adc_if_delay_group") (
+  parameter DEVICE_TYPE = 0) (
 
   // jesd interface
   // rx_clk is (line-rate/40)

library/axi_ad9680/axi_ad9680.v
@@ -40,8 +40,7 @@
 module axi_ad9680 #(
 
   parameter ID = 0,
-  parameter DEVICE_TYPE = 0,
-  parameter IO_DELAY_GROUP = "adc_if_delay_group") (
+  parameter DEVICE_TYPE = 0) (
 
   // jesd interface 
   // rx_clk is (line-rate/40)

topic-embedded-products/adi_hdl

library/axi_ad6676/axi_ad6676.v
@@ -37,8 +37,7 @@
 
 module axi_ad6676 #(
 
-  parameter ID = 0,
-  parameter IO_DELAY_GROUP = "adc_if_delay_group") (
+  parameter ID = 0) (
 
   // jesd interface
   // rx_clk is (line-rate/40)

library/axi_ad9625/axi_ad9625.v
@@ -37,8 +37,7 @@
 
 module axi_ad9625 #(
 
-  parameter ID = 0,
-  parameter IO_DELAY_GROUP = "adc_if_delay_group") (
+  parameter ID = 0) (
 
   // jesd interface
   // rx_clk is (line-rate/40)

library/axi_ad9680/axi_ad9680.v
@@ -37,8 +37,7 @@
 
 module axi_ad9680 #(
 
-  parameter ID = 0,
-  parameter IO_DELAY_GROUP = "adc_if_delay_group") (
+  parameter ID = 0) (
 
   // jesd interface 
   // rx_clk is (line-rate/40)